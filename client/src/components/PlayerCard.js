1b5435bfc586069f019ac99a4234251c914036f5
Vyraal1 <kevin.afable@gmail.com>
2019-10-04 12:03:05 -0400

    initial player card structure

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PlayerCard = props => {
+  console.log("playerCard Props", props.player);
+  const { name, country, searches, id } = props.player;
+  return (
+    <div className="player-card" key={id}>
+      <h2>{name}</h2>
+      <p>{country}</p>
+      <p>Searches: {searches}</p>
+    </div>
+  );
+};
+
+export default PlayerCard;
