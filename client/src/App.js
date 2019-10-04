c9c349555fdf58ff4cd405e26c41d7d1166f4c69
Vyraal1 <kevin.afable@gmail.com>
2019-10-04 12:46:05 -0400

    added hotdog dark mode out of spite

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PlayerList from "./components/PlayerList";
 import "./App.css";
+import { useDarkMode } from "./hooks/useDarkMode";
 
 function App() {
+  const [isDarkMode, setDarkMode] = useDarkMode(false);
+
   return (
     <div className="App">
+      <button onClick={setDarkMode}>Dark Mode Toggle</button>
       <PlayerList />
     </div>
   );
