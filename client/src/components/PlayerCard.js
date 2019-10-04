c944efe2f375dbdca39b6c11e3b5236b7a40f3ef
Vyraal1 <kevin.afable@gmail.com>
2019-10-04 19:10:15 -0400

    reee testing downgraded to meet mvp, testing requests too complicated

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -4,7 +4,7 @@ const PlayerCard = props => {
   console.log("playerCard Props", props.player);
   const { name, country, searches, id } = props.player;
   return (
-    <div className="player-card" key={id}>
+    <div data-testid="player-card" className="player-card" key={id}>
       <h2>{name}</h2>
       <p>{country}</p>
       <p>Searches: {searches}</p>
